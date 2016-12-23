494e085b2292b33ee730a3934f52b4708bc9bb9d
尨喵 <id0o0bi.org@gmail.com>
2016-12-23 19:23:01 +0800

    tests

---
+++ iMooc/7-3.py
@@ -0,0 +1,8 @@
+def square_of_sum(L):
+    sum = 0
+    for i in L:
+        sum += i*i
+    return sum
+
+print square_of_sum([1, 2, 3, 4, 5])
+print square_of_sum([-5, 0, 5, 15, 25])
---
+++ iMooc/7-4.py
@@ -0,0 +1,7 @@
+import math
+
+def quadratic_equation(a, b, c):
+    ???
+
+print quadratic_equation(2, 3, 0)
+print quadratic_equation(1, -6, 5)
