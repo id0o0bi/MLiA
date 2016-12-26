d81290b6303992419798237afdcaf71d24dfe080
尨喵 <id0o0bi.org@gmail.com>
2016-12-26 19:34:01 +0800

    daily commit ...

--- iMooc/7-4.py
+++ iMooc/7-4.py
@@ -1,7 +1,26 @@
 import math
 
 def quadratic_equation(a, b, c):
-    ???
+    x = (-b + math.sqrt(b*b - 4*a*c))/(2*a)
+    y = (-b - math.sqrt(b*b - 4*a*c))/(2*a)
+
+    return x, y
 
 print quadratic_equation(2, 3, 0)
 print quadratic_equation(1, -6, 5)
+
+
+# Better Version
+# import math
+
+# def quadratic_equation(a, b, c):
+#     de=b**2-4*a*c
+#     if de>=0:
+#         x1=(-b+math.sqrt(de))/(2*a)
+#         x2=(-b-math.sqrt(de))/(2*a)
+#         return x1,x2
+#     else:
+#         return
+
+# print quadratic_equation(2, 3, 0)
+# print quadratic_equation(1, -6, 5)
---
+++ iMooc/7-5.py
@@ -0,0 +1,5 @@
+# Tower of Hanoi
+def move(n, a, b, c):
+    ???
+
+move(4, 'A', 'B', 'C')
